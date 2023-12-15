fec653e7863699d85b9dcc2646c12449ffe49fc8
GeroKramar <gerokramar@gmail.com>
2023-12-14 22:34:41 -0300

    commit arreglos 1

--- src/main/java/com/EGG/noticia/configurations/WebSecurity.java
+++ src/main/java/com/EGG/noticia/configurations/WebSecurity.java
@@ -7,36 +7,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurity {
-    
-    @Autowired
-    private UsuarioService us;
-    
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(us).passwordEncoder(new BCryptPasswordEncoder());
-    }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/**").permitAll()
-                .formLogin((login) -> login
-                        .loginPage("/login")
-                        .usernameParameter("userName"))
-                .logout((logout) -> logout
-                        .logoutUrl("/logout"))
-                .csrf((csrf) -> csrf.disable()));
-        return http.build();
-    }
+        @Autowired
+        public UsuarioService usuarioServices;
+
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+                auth.userDetailsService(usuarioServices).passwordEncoder(new BCryptPasswordEncoder());
+        }
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests((authz) -> authz
+                                                .requestMatchers("/**").permitAll())
+                                .formLogin((login) -> login
+                                                .loginPage("/login")
+                                                .usernameParameter("Username"))
+                                .logout((logout) -> logout
+                                                .logoutUrl("/logout"))
+                                .csrf((csrf) -> csrf.disable());
+                return http.build();
+        }
 
 }
--- src/main/java/com/EGG/noticia/services/UsuarioService.java
+++ src/main/java/com/EGG/noticia/services/UsuarioService.java
@@ -11,12 +11,49 @@ import com.EGG.noticia.enums.Rol;
 import com.EGG.noticia.exceptions.MyException;
 import com.EGG.noticia.repositories.UsuarioRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.Transactional;
+
+
+
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService {
 
     @Autowired
-    private UsuarioRepository ur;
+    private UsuarioRepository userRepo;
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Usuario usuario = userRepo.obtenerPorEmail(email);
+
+        if(usuario != null){
+            List<GrantedAuthority> permisos = new ArrayList<>();
+            GrantedAuthority p = new SimpleGrantedAuthority("ROLE_" + usuario.getRol().toString());
+            permisos.add(p);
+            ServletRequestAttributes atrib = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+            HttpSession sesion = atrib.getRequest().getSession();
+            return new org.springframework.security.core.userdetails.User(usuario.getEmail(), usuario.getPassword(), permisos);
+        }
+        return null;
+
+    }
+@Transactional
     public void crearUsuario(String nombreUsuario, String email, String password) throws MyException {
 
         validarStr(nombreUsuario);
@@ -32,48 +69,48 @@ public class UsuarioService {
         u.setRol(Rol.USER);
         u.setActivo(true);
 
-        ur.save(u);
+        userRepo.save(u);
     }
-
+@Transactional
     public void modificarNombreUsuario(Long id, String nombreUsuario) throws MyException{
         validarStr(nombreUsuario);
         validarLong(id);
-        Optional<Usuario> res = ur.findById(id);
+        Optional<Usuario> res = userRepo.findById(id);
         if(res.isPresent()){
             Usuario u = res.get();
             u.setNombreUsuario(nombreUsuario);
-            ur.save(u);
+            userRepo.save(u);
         }
     }
-
+@Transactional
     public void modificarEmail(Long id, String email) throws MyException{
         validarStr(email);
         validarLong(id);
-        Optional<Usuario> res = ur.findById(id);
+        Optional<Usuario> res = userRepo.findById(id);
         if(res.isPresent()){
             Usuario u = res.get();
             u.setEmail(email);
-            ur.save(u);
+            userRepo.save(u);
         }
     }
-    
+ @Transactional   
     public void modificarPassword(Long id, String password) throws MyException{
         validarStr(password);
         validarLong(id);
-        Optional<Usuario> res = ur.findById(id);
+        Optional<Usuario> res = userRepo.findById(id);
         if(res.isPresent()){
             Usuario u = res.get();
             u.setPassword(password);
-            ur.save(u);
+            userRepo.save(u);
         }
     }
-
+@Transactional
     public void eliminarUsuario(Long id) throws MyException{
         validarLong(id);
-        Optional<Usuario> res = ur.findById(id);
+        Optional<Usuario> res = userRepo.findById(id);
         if(res.isPresent()){
             Usuario u = res.get();
-            ur.delete(u);
+            userRepo.delete(u);
         }
     }
     
